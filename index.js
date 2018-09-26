7c44a9fecbb2382903f736b21491cc4cd05fba53
Kate Rentschler <nkpe@outlook.com>
2018-09-26 20:40:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ var topic = "space";
 var conversation = `${topic}`;
 var greeting = "Hello, everybody!"
 var greetSpecialGuest = `Hello, ${specialGuest}!`
+var convertation = `Let's talk about ${topic}.`
